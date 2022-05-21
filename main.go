f4cd8cd8e239b2cea76685048f27f73d2b1e4a8f
henry.tu <henry.tu@caloudi.com>
2022-05-22 00:46:15 +0800

    revised env

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	_ "main/docs"
-	"os"
 )
 
 // @securityDefinitions.apikey ApiKeyAuth
 // @in header
 // @name Authorization
 func main() {
-	os.Setenv("SQLCONNECTSTRING", "root:@tcp(20.99.156.107:3306)/godev")
+	//os.Setenv("SQLCONNECTSTRING", "root:@tcp(20.99.156.107:3306)/godev")
 	router := newRouter()
 	router.Logger.Fatal(router.Start(":80"))
 }

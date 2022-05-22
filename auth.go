83e9ceb5452ad926d3e6ae9f82c5bc0646ec1025
henry.tu <henry.tu@caloudi.com>
2022-05-22 22:28:47 +0800

    modulize model layer, revised auth, added homepage

--- auth.go
+++ auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"main/model"
+	"main/util"
 	"net/http"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 
 var signingKey = []byte("secret")
 
+// @Tags         Token
 // @Description login user
 // @Accept  json
 // @Param login body object true "json"
@@ -30,10 +32,15 @@ func login(c echo.Context) error {
 			Message: "empty name",
 		}
 	}
-	user := model.FindUser(&model.UserLoginPost{Account: u.Account})
-	if user.Account != u.Account || user.Password != u.Password {
+	user, err := model.FindUser(util.GetSQLConnectString(), &model.UserLoginPost{Account: u.Account})
+	if err != nil {
 		return &echo.HTTPError{
-			Code:    http.StatusUnauthorized,
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		}
+	} else if user.Account != u.Account || user.Password != u.Password {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
 			Message: "invalid name or password",
 		}
 	}
@@ -57,6 +64,7 @@ func login(c echo.Context) error {
 	})
 }
 
+// @Tags         Token
 // @Description create user
 // @Accept  json
 // @Param signup body object true "json"
@@ -76,24 +84,32 @@ func signup(c echo.Context) error {
 		}
 	}
 
-	if u := model.FindUser(&model.UserLoginPost{Account: user.Account}); u.Account == user.Account {
+	u, err := model.FindUser(util.GetSQLConnectString(), &model.UserLoginPost{Account: user.Account})
+	if err != nil {
 		return &echo.HTTPError{
 			Code:    http.StatusConflict,
+			Message: err.Error(),
+		}
+	} else if u.Account == user.Account {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
 			Message: "name already exists",
 		}
 	}
-	model.CreateUser(user)
+
+	model.CreateUser(util.GetSQLConnectString(), user)
 	return c.JSON(http.StatusCreated, user)
 }
 
-// @Description test
+// @Tags         Token
+// @Description getAccountFromJWT
 // @Accept  json
 // @Param user path string true "token"
 // @Success 200 "ok"
 // @Failure 500 "error"
-// @Router /api/test [get]
+// @Router /api/getAccountFromJWT [get]
 // @security securityDefinitions.apikey ApiKeyAuth
-func test(c echo.Context) error {
+func getAccountFromJWT(c echo.Context) error {
 	user := c.Get("user").(*jwt.Token)
 	claims := user.Claims.(*jwtCustomClaims)
 	account := claims.Account

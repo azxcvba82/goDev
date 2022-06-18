fede4b8506e60a7ed5ba3177f4aab4bc34c68a3f
henry.tu <henry.tu@caloudi.com>
2022-06-18 13:06:06 +0800

    using readwirte split

--- util/common.go
+++ util/common.go
@@ -9,6 +9,10 @@ func GetSQLConnectString() string {
 	return os.Getenv("SQLCONNECTSTRING")
 }
 
+func GetSQLConnectStringRead() string {
+	return os.Getenv("SQLCONNECTSTRINGREAD")
+}
+
 func CheckErr(err error) {
 	if err != nil {
 		fmt.Println(err.Error())

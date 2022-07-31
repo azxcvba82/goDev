09ced28fe940543c165df7c80cdf2b72e8ed2fca
Henry Tu <60205901+azxcvba82@users.noreply.github.com>
2022-07-31 22:13:27 +0800

    Develop (#39)
    * rev
    
    * fixed swagger
    
    * Update docker-image.yml
    
    * add sql test
    
    * resove conflit
    
    * added login func
    
    * fixed error
    
    * change secret moethod
    
    * add change
    
    * add jwt
    
    * revised env
    
    * ok (#13)
    
    * Develop (#12)
    
    * rev
    
    * fixed swagger
    
    * Update docker-image.yml
    
    * add sql test
    
    * resove conflit
    
    * added login func
    
    * fixed error
    
    * change secret moethod
    
    * add change
    
    * add jwt
    
    * revised env
    
    Co-authored-by: henry.tu <henry.tu@caloudi.com>
    
    * Update main.go
    
    Co-authored-by: henry.tu <henry.tu@caloudi.com>
    
    * change name
    
    * modulize model layer, revised auth, added homepage
    
    * remove code
    
    * added infra image
    
    * rev
    
    * add endpoint
    
    * add album
    
    * add kind
    
    * added getAlbumById
    
    * added getAlbumsByKindId
    
    * added search logic
    
    * add search & products
    
    * added AlbumType
    
    * added getShoppingCartByAccount
    
    * added auth expire time
    
    * added join data
    
    * add album search
    
    * using readwirte split
    
    * added adv search
    
    * added playList
    
    * null handel
    
    * added fAlbumName
    
    * added fCoverPath
    
    * added add, delete playList
    
    * add route
    
    * revised sqlRead
    
    * added cache
    
    * add redis clinet
    
    * added 2nd sql cache by redis
    
    * revised  Album query
    
    Co-authored-by: henry.tu <henry.tu@caloudi.com>

--- model/Album.go
+++ model/Album.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"database/sql"
 	"errors"
 	"main/util"
 
@@ -25,93 +24,27 @@ type Album struct {
 
 func AllAlbum(sqlConnectionString string) (model []Album, err error) {
 	var albums []Album
-	//nullfActivityID := new(sql.NullInt32)
 	queryString := `SELECT fAlbumID, fAlbumName, fMaker, fAccount, fYear, fType, fStatus, fALPrice, fCoverPath, fKinds, fDiscount, fActivityID  FROM tAlbum WHERE fStatus = 2 `
-	//rows, err := util.SQLQuery(sqlConnectionString, queryString)
-	err = util.SQLQueryV2(&albums, sqlConnectionString, queryString)
+	err = util.SQLQueryV2(&albums, sqlConnectionString, true, queryString)
 
 	if err != nil {
 		return albums, err
 	}
 
-	// for rows.Next() {
-	// 	var obj Album
-	// 	var fAlbumID int
-	// 	var fAlbumName string
-	// 	var fMaker string
-	// 	var fAccount string
-	// 	var fYear string
-	// 	var fType int
-	// 	var fStatus int
-	// 	var fALPrice float32
-	// 	var fCoverPath string
-	// 	var fKinds string
-	// 	var fDiscount float32
-	// 	err = rows.Scan(&fAlbumID, &fAlbumName, &fMaker, &fAccount, &fYear, &fType, &fStatus, &fALPrice, &fCoverPath, &fKinds, &fDiscount, nullfActivityID)
-
-	// 	util.CheckErr(err)
-	// 	if nullfActivityID.Valid {
-	// 		obj.ActivityID = int(nullfActivityID.Int32)
-	// 	}
-	// 	obj.AlbumID = fAlbumID
-	// 	obj.AlbumName = fAlbumName
-	// 	obj.Maker = fMaker
-	// 	obj.Account = fAccount
-	// 	obj.Year = fYear
-	// 	obj.Type = fType
-	// 	obj.Status = fStatus
-	// 	obj.ALPrice = fALPrice
-	// 	obj.CoverPath = fCoverPath
-	// 	obj.Kinds = fKinds
-	// 	obj.Discount = fDiscount
-	// 	albums = append(albums, obj)
-	// }
 	return albums, nil
 }
 
 func GetAlbumById(sqlConnectionString string, albumId string) (model Album, err error) {
 	var album Album
-	nullfActivityID := new(sql.NullInt32)
-	queryString := `SELECT *  FROM tAlbum WHERE fAlbumID = ? LIMIT 1`
-	rows, err := util.SQLQuery(sqlConnectionString, queryString, albumId)
+	queryString := `SELECT fAlbumID, fAlbumName, fMaker, fAccount, fYear, fType, fStatus, fALPrice, fCoverPath, fKinds, fDiscount, fActivityID  FROM tAlbum WHERE fAlbumID = ? LIMIT 1`
+	err = util.SQLQueryV2(&album, sqlConnectionString, true, queryString, albumId)
 
 	if err != nil {
 		return album, err
 	}
 
-	counter := 0
-	for rows.Next() {
-		var fAlbumID int
-		var fAlbumName string
-		var fMaker string
-		var fAccount string
-		var fYear string
-		var fType int
-		var fStatus int
-		var fALPrice float32
-		var fCoverPath string
-		var fKinds string
-		var fDiscount float32
-		err = rows.Scan(&fAlbumID, &fAlbumName, &fMaker, &fAccount, &fYear, &fType, &fStatus, &fALPrice, &fCoverPath, &fKinds, &fDiscount, nullfActivityID)
-
-		util.CheckErr(err)
-		if nullfActivityID.Valid {
-			//album.ActivityID = int(nullfActivityID.Int32)
-		}
-		album.AlbumID = fAlbumID
-		album.AlbumName = fAlbumName
-		album.Maker = fMaker
-		album.Account = fAccount
-		album.Year = fYear
-		album.Type = fType
-		album.Status = fStatus
-		album.ALPrice = fALPrice
-		album.CoverPath = fCoverPath
-		album.Kinds = fKinds
-		album.Discount = fDiscount
-		counter++
-	}
-	if counter == 0 {
+	var albumForCheck Album
+	if album == albumForCheck {
 		return album, errors.New("select id not found")
 	}
 	return album, nil
@@ -119,49 +52,14 @@ func GetAlbumById(sqlConnectionString string, albumId string) (model Album, err 
 
 func GetAlbumsByKindId(sqlConnectionString string, kindId string) (model []Album, err error) {
 	var albums []Album
-	nullfActivityID := new(sql.NullInt32)
-	queryString := `SELECT * FROM tAlbum where fStatus = 2 AND fkinds LIKE CONCAT('%',(SELECT fKindName FROM tAlbumKind WHERE fKindID = ?),'%') ORDER BY fYear DESC; `
-	rows, err := util.SQLQuery(sqlConnectionString, queryString, kindId)
+	queryString := `SELECT fAlbumID, fAlbumName, fMaker, fAccount, fYear, fType, fStatus, fALPrice, fCoverPath, fKinds, fDiscount, fActivityID FROM tAlbum where fStatus = 2 AND fkinds LIKE CONCAT('%',(SELECT fKindName FROM tAlbumKind WHERE fKindID = ?),'%') ORDER BY fYear DESC; `
+	err = util.SQLQueryV2(&albums, sqlConnectionString, true, queryString, kindId)
 
 	if err != nil {
 		return albums, err
 	}
 
-	counter := 0
-	for rows.Next() {
-		var obj Album
-		var fAlbumID int
-		var fAlbumName string
-		var fMaker string
-		var fAccount string
-		var fYear string
-		var fType int
-		var fStatus int
-		var fALPrice float32
-		var fCoverPath string
-		var fKinds string
-		var fDiscount float32
-		err = rows.Scan(&fAlbumID, &fAlbumName, &fMaker, &fAccount, &fYear, &fType, &fStatus, &fALPrice, &fCoverPath, &fKinds, &fDiscount, nullfActivityID)
-
-		util.CheckErr(err)
-		if nullfActivityID.Valid {
-			//obj.ActivityID = int(nullfActivityID.Int32)
-		}
-		obj.AlbumID = fAlbumID
-		obj.AlbumName = fAlbumName
-		obj.Maker = fMaker
-		obj.Account = fAccount
-		obj.Year = fYear
-		obj.Type = fType
-		obj.Status = fStatus
-		obj.ALPrice = fALPrice
-		obj.CoverPath = fCoverPath
-		obj.Kinds = fKinds
-		obj.Discount = fDiscount
-		albums = append(albums, obj)
-		counter++
-	}
-	if counter == 0 {
+	if albums == nil {
 		return albums, errors.New("select id not found")
 	}
 	return albums, nil

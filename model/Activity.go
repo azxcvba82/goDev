991db94e32d27878666f10e6265a2b5f9ecaf5d5
henry.tu <henry.tu@caloudi.com>
2022-08-03 22:57:39 +0800

    revised Activity

--- model/Activity.go
+++ model/Activity.go
@@ -1,56 +1,35 @@
 package model
 
 import (
-	"database/sql"
 	"main/util"
+
+	"gopkg.in/guregu/null.v4"
 )
 
 type Activity struct {
-	Id        int    `int:"Id"`
-	Launcher  string `string:"Launcher"`
-	StartTime string `string:"StartTime"`
-	EndTime   string `string:"EndTime"`
-	Title     string `string:"Title"`
-	PhotoPath string `string:"PhotoPath"`
+	Id        int         `db:"fId"`
+	Launcher  null.String `db:"fLauncher"`
+	StartTime string      `db:"fStartTime"`
+	EndTime   string      `db:"fEndTime"`
+	Title     string      `db:"fTitle"`
+	PhotoPath string      `db:"fPhotoPath"`
 }
 
 func EventQuery(sqlConnectionString string, account string) (model []Activity, err error) {
 	var activities []Activity
-	nullfLauncher := new(sql.NullString)
 	queryString := ""
-	var rows *sql.Rows
 
 	if account == "" {
-		queryString = `SELECT *  FROM tActivity `
-		rows, err = util.SQLQuery(sqlConnectionString, queryString)
+		queryString = `SELECT fId, fLauncher, fStartTime, fEndTime, fTitle, fPhotoPath  FROM tActivity `
+		err = util.SQLQueryV2(&activities, sqlConnectionString, true, queryString)
 	} else {
-		queryString = `SELECT *  FROM tActivity WHERE fLauncher = ? `
-		rows, err = util.SQLQuery(sqlConnectionString, queryString, account)
+		queryString = `SELECT fId, fLauncher, fStartTime, fEndTime, fTitle, fPhotoPath  FROM tActivity WHERE fLauncher = ? `
+		err = util.SQLQueryV2(&activities, sqlConnectionString, true, queryString, account)
 	}
 
 	if err != nil {
 		return activities, err
 	}
 
-	for rows.Next() {
-		var obj Activity
-		var fId int
-		var fStartTime string
-		var fEndTime string
-		var fTitle string
-		var fPhotoPath string
-		err = rows.Scan(&fId, nullfLauncher, &fStartTime, &fEndTime, &fTitle, &fPhotoPath)
-
-		util.CheckErr(err)
-		if nullfLauncher.Valid {
-			obj.Launcher = nullfLauncher.String
-		}
-		obj.Id = fId
-		obj.StartTime = fStartTime
-		obj.EndTime = fEndTime
-		obj.Title = fTitle
-		obj.PhotoPath = fPhotoPath
-		activities = append(activities, obj)
-	}
 	return activities, nil
 }

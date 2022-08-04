205b2bc175b615ceb42f5b123e72cdfddf780591
henry.tu <henry.tu@caloudi.com>
2022-08-04 21:15:31 +0800

    revised kind

--- model/Kind.go
+++ model/Kind.go
@@ -1,39 +1,28 @@
 package model
 
 import (
+	"errors"
 	"main/util"
 )
 
 type Kind struct {
-	KindID    int    `int:"KindID"`
-	KindName  string `string:"KindName"`
-	Color     string `string:"Color"`
-	PhotoPath string `string:"PhotoPath"`
+	KindID    int    `db:"fKindID"`
+	KindName  string `db:"fKindName"`
+	Color     string `db:"fColor"`
+	PhotoPath string `db:"fPhotoPath"`
 }
 
 func AllKind(sqlConnectionString string) (model []Kind, err error) {
 	var kinds []Kind
-	queryString := `SELECT *  FROM tAlbumKind `
-	rows, err := util.SQLQuery(sqlConnectionString, queryString)
+	queryString := `SELECT fKindID, fKindName, fColor, fPhotoPath  FROM tAlbumKind `
+	err = util.SQLQueryV2(&kinds, sqlConnectionString, true, queryString)
 
 	if err != nil {
 		return kinds, err
 	}
 
-	for rows.Next() {
-		var obj Kind
-		var fKindID int
-		var fKindName string
-		var fColor string
-		var fPhotoPath string
-		err = rows.Scan(&fKindID, &fKindName, &fColor, &fPhotoPath)
-
-		util.CheckErr(err)
-		obj.KindID = fKindID
-		obj.KindName = fKindName
-		obj.Color = fColor
-		obj.PhotoPath = fPhotoPath
-		kinds = append(kinds, obj)
+	if kinds == nil {
+		return kinds, errors.New("select not found")
 	}
 	return kinds, nil
 }

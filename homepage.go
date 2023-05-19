8fad1d6e0182ef330943229df2cad43bdf6cc167
azxcvba82 <azxcvba82@gmail.com>
2023-05-19 22:32:52 +0800

    added flushAllCache

--- homepage.go
+++ homepage.go
@@ -39,3 +39,21 @@ func mainAlbums(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, album)
 }
+
+// @Tags         Homepage
+// @Description Flush All Cache
+// @Success 200 "ok"
+// @Failure 500 "error"
+// @Router /api/flushAllCache [get]
+// @security securityDefinitions.apikey BearerAuth
+// @security BearerAuth
+func flushAllCache(c echo.Context) error {
+	err := util.FlushAllCache()
+	if err != nil {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		}
+	}
+	return c.JSON(http.StatusOK, "done")
+}
